82b6850a00736ec3132e47b2d1665b631cd25a28
ddukbaek2 <ddukbaek2@gmail.com>
2026-03-27 18:43:49 +0900

    애니메이션 테스트.

--- tutorial.js
+++ tutorial.js
@@ -10,6 +10,42 @@ import { Renderer } from "./src/core/renderer.js";
 import { Scene } from "./src/core/scene.js";
 import { ViewScaleMode } from "./src/core/viewmanager.js";
 import { Animation } from "./src/rendering/animation.js";
+import { ImageAsset } from "./src/resource/imageasset.js";
+import { Frame } from "./src/core/frame.js";
+
+
+//==============================================================================
+// 한장의 이미지를 통해 애니메이션 프레임 목록을 만들어 반환.
+//==============================================================================
+/**
+ * 
+ * @param { HTMLImageElement } image
+ * @param { number } columns
+ * @param { number } rows
+ * @param { number } count
+ * @returns { Frame[] }
+ */
+export function createFramesFromRects(image, columns, rows, count) {
+	const frameWidth = image.width / columns;
+	const frameHeight = image.height / rows;
+
+	// 모든 프레임 좌표 생성.
+	const frames = [];
+	for (let y = 0; y < rows; ++y) {
+		for (let x = 0; x < columns; ++x) {
+			const rect = Rect.create(x * frameWidth, y * frameHeight, frameWidth, frameHeight);
+			// console.table(rect);
+
+			const frame = new Frame(image, rect);
+			frames.push(frame);
+			if (frames.length === count) {
+				return frames;
+			}
+		}
+	}	
+
+	return frames;
+}
 
 
 //==============================================================================
@@ -20,6 +56,29 @@ class Tutorial extends Scene {
 	// 멤버 변수 목록.
 	//==============================================================================
 	/** @private @type { Animation } */ #animation;
+
+	//==============================================================================
+	// 불러오기.
+	//==============================================================================
+	/**
+	 * @param { Engine } engine 
+	 */
+	async load(engine) {
+		await super.load(engine);
+
+		// 이미지 불러오기.
+		const imageAsset = new ImageAsset();
+		await imageAsset.load("./spritesheet.png");
+		// await imageAsset.load("./effect_3.png");
+		
+		// 모든 프레임 좌표 생성.
+		// const frames = createFramesFromRects(imageAsset.image, 1, 6, 6);
+		const frames = createFramesFromRects(imageAsset.image, 3, 3, 8);
+		this.#animation = new Animation();
+		this.#animation.setFrames(frames);
+		this.#animation.setAnimationSpeed(8);
+		this.#animation.setLoop(true);
+	}
 
 	//==============================================================================
 	// 초기화.
@@ -35,9 +94,7 @@ class Tutorial extends Scene {
 		// viewManager.setViewScaleMode(ViewScaleMode.referenceResolution); // 기준 해상도.
 		// viewManager.setViewScaleMode(ViewScaleMode.stretchWidth); // 기준해상도 + 가로로 늘려붙이기.
 		viewManager.setViewScaleMode(ViewScaleMode.stretchHeight);
-		viewManager.setViewScaleMode(ViewScaleMode.stretchAuto);
-
-		this.#animation = new Animation();
+		// viewManager.setViewScaleMode(ViewScaleMode.stretchAuto);
 	}
 
 	//==============================================================================
@@ -49,6 +106,8 @@ class Tutorial extends Scene {
 	tick(timeDelta) {
 		super.tick(timeDelta);
 		this.#animation.tick(timeDelta);
+		const currentFrameIndex = this.#animation.getCurrentFrameIndex();
+		// console.log(`currentFrameIndex: ${currentFrameIndex}`);
 	}
 
 	//==============================================================================
@@ -76,20 +135,33 @@ class Tutorial extends Scene {
 		viewManager.applyViewRect(canvasContext);
 		renderer.drawRect(Rect.create(0, 0, referenceResolutionSize.x, referenceResolutionSize.y), Colors.lightVanilla);
 
-		// 사각형 그리기.
-		let boxPosition = Vec2.create(0, 0);
-		let boxSize = Vec2.create(100, 100);
-		boxPosition = boxPosition.add(referenceResolutionSize.divide(2)).subtract(boxSize.divide(2));
-		renderer.drawRect(Rect.create(boxPosition.x, boxPosition.y, boxSize.x, boxSize.y), "#ffff00");
+		// // 사각형 그리기.
+		// let boxPosition = Vec2.create(0, 0);
+		// let boxSize = Vec2.create(100, 100);
+		// boxPosition = boxPosition.add(referenceResolutionSize.divide(2)).subtract(boxSize.divide(2));
+		// renderer.drawRect(Rect.create(boxPosition.x, boxPosition.y, boxSize.x, boxSize.y), "#ffff00");
+
+		// 애니메이션 그리기.
+		const frame = this.#animation.getCurrentFrame();
+		if (frame) {
+			const image = frame.getImage();
+			const rect = frame.getRect();
+			const position = referenceResolutionSize.divide(2).subtract(rect.size.divide(2));
+			renderer.drawImage(image, position, rect.size, rect);
+		}
 	}
 
+	//==============================================================================
+	// 터치 누름.
+	//==============================================================================
+	/**
+	 * @param { Vector2 } viewInputPosition
+	 */
 	touchPress(viewInputPosition) {
-		const engine = super.getEngine();
-		const viewManager = engine.getViewManager();
+		super.touchPress(viewInputPosition);
 
-		// this.#animation.setFramesFromImages();
-		this.#animation.setAnimationSpeed(10);
-		this.#animation.play();
+		// 애니메이션 실행.
+		this.#animation.gotoAndPlay(0);
 	}
 }
 
