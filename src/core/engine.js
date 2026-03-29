85402d9b8fafe32267177a2481f1f95af99d36ce
ddukbaek2 <ddukbaek2@gmail.com>
2026-03-30 02:17:59 +0900

    버전 업.

--- src/core/engine.js
+++ src/core/engine.js
@@ -80,7 +80,7 @@ export class Engine extends Object {
 
 		this.#resizeCallback = this.resize.bind(this);
 		this.#updateEngineCallback = this.updateEngine.bind(this);
-		this.#version = Version.create(0, 0, 6);
+		this.#version = Version.create(0, 0, 7);
 
 		// 이벤트 설정.
 		this.setupAllDocumentEvents();
