deb4b494b6a593d90064d21ed895af7b2a032cf1
ddukbaek2 <ddukbaek2@gmail.com>
2026-04-01 22:27:20 +0900

    비동기 1프레임 대기 추가.

--- src/base/wait.js
+++ src/base/wait.js
@@ -16,3 +16,13 @@ export async function seconds(seconds) {
 	const promise = new System.Promise(resolve => System.setTimeout(resolve, seconds * 1000));
 	await promise;
 }
+
+//==============================================================================
+// 한 프레임 비동기 대기.
+//==============================================================================
+/**
+ * @returns { System.Promise<void> }
+ */
+export async function nextFrame() {
+	new Promise(resolve => System.window.requestAnimationFrame(resolve));
+}
