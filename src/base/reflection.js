d8bd41e76129a09e13068472b679f738d8cba8af
ddukbaek2 <ddukbaek2@gmail.com>
2026-04-01 14:47:58 +0900

    수정.

--- src/base/reflection.js
+++ src/base/reflection.js
@@ -13,14 +13,40 @@ const System = globalThis;
  * @returns { T }
  */
 export function clone(target) {
+	// 오류 방어.
+	if (target === null || typeof target !== "object") {
+		return target;
+	}
+
 	// 생성자로 신규 객체 생성 후, 보유한 멤버를 원시 타입은 덮어씌우고 참조 타입은 얕은 복사.
 	// const obj = new this.constructor();
 	// System.Object.assign(new this.constructor(), target);
 	// return obj;
 
-	// 생성자 호출을 우회하여 신규 객체 생성 후, 보유한 멤버를 원시 타입은 덮어씌우고 참조 타입은 얕은 복사.
-	const obj = System.Object.create(System.Object.getPrototypeOf(target));
+	// // 생성자 호출을 우회하여 신규 객체 생성 후, 보유한 멤버를 원시 타입은 덮어씌우고 참조 타입은 얕은 복사.
+	// const obj = System.Object.create(System.Object.getPrototypeOf(target));
+
+	// 생성자 호출을 통해 신규 객체 생성 (프라이빗 멤버 정상 할당).
+	const obj = new target.constructor();
+
+	// 1. 객체 자신의 열거 가능한 속성 얕은 복사.
 	System.Object.assign(obj, target);
+
+	// 2. 클래스 프로토타입에 정의된 getter/setter를 통한 프로퍼티 복사 (프라이빗 변수 접근).
+	let currentProto = System.Object.getPrototypeOf(target);
+	while (currentProto && currentProto !== System.Object.prototype) {
+		const props = System.Object.getOwnPropertyNames(currentProto);
+		for (const prop of props) {
+			if (prop !== "constructor") {
+				const descriptor = System.Object.getOwnPropertyDescriptor(currentProto, prop);
+				if (descriptor && descriptor.get && descriptor.set) {
+					obj[prop] = target[prop];
+				}
+			}
+		}
+		currentProto = System.Object.getPrototypeOf(currentProto);
+	}
+
 	return obj;
 }
 
@@ -39,13 +65,16 @@ export function structuredClone(target) {
 	if (target === null || typeof target !== "object")
 		throw new System.Error();
 
-	const obj = System.Object.create(System.Object.getPrototypeOf(target));
+	// 생성자 호출을 통해 신규 객체 생성 (프라이빗 멤버 정상 할당).
+	// const obj = System.Object.create(System.Object.getPrototypeOf(target));
+	const obj = new target.constructor();
 
 	// 깊은 복사 함수는 직렬화 불가능 객체를 만나면 예외 발생됨.
 	//System.Object.assign(obj, System.window.structuredClone(this));
 
 	// 값 복사 재귀 함수.
 	const deepCopy = (destination, source) => {
+		// 1. 객체 자신의 열거 가능한 속성 깊은 복사.
 		for (const name in source) {
 
 			// 멤버 실제 존재 여부.
@@ -69,6 +98,32 @@ export function structuredClone(target) {
 			else {
 				destination[name] = value;
 			}
+		}
+
+		// 2. 클래스 프로토타입에 정의된 getter/setter를 통한 프로퍼티 복사 (프라이빗 변수 접근).
+		let currentProto = System.Object.getPrototypeOf(source);
+		while (currentProto && currentProto !== System.Object.prototype) {
+			const props = System.Object.getOwnPropertyNames(currentProto);
+			for (const prop of props) {
+				if (prop !== "constructor") {
+					const descriptor = System.Object.getOwnPropertyDescriptor(currentProto, prop);
+					if (descriptor && descriptor.get && descriptor.set) {
+						const value = source[prop];
+						if (System.Array.isArray(value)) {
+							destination[prop] = [];
+							deepCopy(destination[prop], value);
+						}
+						else if (value !== null && typeof value === "object" && value.constructor === System.Object) {
+							destination[prop] = {};
+							deepCopy(destination[prop], value);
+						}
+						else {
+							destination[prop] = value;
+						}
+					}
+				}
+			}
+			currentProto = System.Object.getPrototypeOf(currentProto);
 		}
 	};
 
