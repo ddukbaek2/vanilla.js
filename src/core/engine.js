ec4ca266f8a4a327d1c4e6d618dc163bd96e7a12
ddukbaek2 <ddukbaek2@gmail.com>
2026-04-13 06:44:17 +0900

    버전업.

--- src/core/engine.js
+++ src/core/engine.js
@@ -102,7 +102,7 @@ export class Engine extends Object {
 		this.#updateEngineCallback = this.updateEngine.bind(this);
 
 		this.#statisticsTextRect = Rect.zero();
-		this.#version = Version.create(0, 1, 0);
+		this.#version = Version.create(0, 2, 0);
 
 		// 이벤트 설정.
 		this.setupAllDocumentEvents();
